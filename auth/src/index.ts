021b0a61b54c532785d11a6fa6d314bf99ce1639
kimji <kimjigabk@gmail.com>
2022-05-28 12:28:45 -0400

    ingress-nginx setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/current_user', (req, res) => {
+  res.send('Hi there');
+});
+
 app.listen(3000, () => {
   console.log('Auth: port 3000@@@@');
 });
