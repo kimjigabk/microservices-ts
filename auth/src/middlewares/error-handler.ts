97203e140ca6ab5e08d1879a0b341edb1461eed7
kimji <kimjigabk@gmail.com>
2022-05-28 14:25:09 -0400

    Error handling middleware

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
+
+// Error response structure
+// {
+//     errors: {
+//         message: string, field?:string
+//     }[]
+// }
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+
+    return res.status(400).send({ errors: formattedErrors });
+  }
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
+  res.status(400).send({ errors: [{ message: 'Something went wrong' }] });
+};
