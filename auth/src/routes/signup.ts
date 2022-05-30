281c72ec3c70ab96a85bad746101247f924ea666
kimji <kimjigabk@gmail.com>
2022-05-29 22:54:37 -0400

    store jwt in session on signn up

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { BadRequestError } from '../errors/bad-request-error';
 // import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
@@ -39,6 +40,18 @@ router.post(
     await user.save();
 
     // Login, Send token
+    // Generate Token
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+    // Store it to Session
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
